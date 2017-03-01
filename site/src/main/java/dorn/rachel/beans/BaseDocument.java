4d73ff2b33264e15190fccff402eb36b72e8a5e0
rachd <rachel.m.dorn@gmail.com>
2017-03-01 14:39:26 -0500

    set up project

---
+++ site/src/main/java/dorn/rachel/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package dorn.rachel.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="helloHippo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
